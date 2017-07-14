a3b1e9fc6d4fa079119db4bb53a1da15f21a1f54
zfreego <zfreego@live.com>
2017-07-14 19:15:22 +0800

    删去直流合闸控制

--- ZFreeGo.ChoicePhase.PlatformModel/DataItemSet/PhaseChoice.cs
+++ ZFreeGo.ChoicePhase.PlatformModel/DataItemSet/PhaseChoice.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using ZFreeGo.ChoicePhase.DeviceNet.LogicApplyer;
+using ZFreeGo.ChoicePhase.PlatformModel.GetViewData;
 
 namespace ZFreeGo.ChoicePhase.PlatformModel.DataItemSet
 {
@@ -756,6 +757,11 @@ namespace ZFreeGo.ChoicePhase.PlatformModel.DataItemSet
 
                 PhaseItemI = PhaseNull;
                 UpdateAngleVisble();
+
+                if(NodeAttribute.SingleMode)
+                {
+                    PhaseItemI = PhaseA;
+                }
             }
             catch(Exception ex)
             {
